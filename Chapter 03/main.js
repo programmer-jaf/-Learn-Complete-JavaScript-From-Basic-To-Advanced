cc9da9b195b6b36af98a18e57c5b0d266fdf5001
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-15 12:12:54 +0600

    JavaScript variables and datatypes explanation

---
+++ Chapter 03/main.js
@@ -0,0 +1,45 @@
+/*
+  Variables
+  Datatypes
+*/
+
+// What is Variables
+
+// Ans: Variables are container to store values. Example: If you want to store milk you have to store it into a Milk-bottle 🍼. Here 🍼 is a variable
+
+var variables = 10;
+console.log(variables);
+
+// Datatypes
+
+// Ans: There are 8 datatypes in JavaScript
+
+// 1. Number
+var Number = 5;
+console.log(Number);
+// 2. String
+var String = "Hello World";
+console.log(String);
+// 3. Boolean
+var Boolean = true;
+console.log(Boolean);
+// 4. Null
+var Null = null;
+console.log(Null);
+
+// 5. Undefined
+var Undefined;
+console.log(Undefined);
+// 6. BigInt
+var BigInt = 9007199254740991n;
+console.log(BigInt);
+// 7. Symbol
+var symbol = Symbol("My Symbol");
+console.log(symbol);
+// 8. Object
+var Object = { name: "John Doe" };
+console.log(Object);
+
+// Check the data-type
+
+console.log(typeof variables);
